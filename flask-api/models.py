bc3df03757d90a302a5f465714693f14b2ec84ff
Karma Soliman <karma.soliman@epita.fr>
2026-02-02 13:05:12 +0100

    django schema preparations

--- flask-api/models.py
+++ flask-api/models.py
@@ -3,6 +3,6 @@ from .extensions import db
 
 class Story(db.Model):
     __tablename__ = 'story'
-    story_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), unique=True, nullable=False, index=True)
-    description = db.column(db.Text, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False,)
+    description = db.column(db.Text, nullable=True)
